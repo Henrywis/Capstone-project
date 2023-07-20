95269af7c2238b7721ca74200e6f6314528e7390
Henrywis <alumonahenry7@gmail.com>
2023-07-20 12:47:03 -0700

    Fixing merge & branch issues

--- iRecruit-express-app/models/post.js
+++ iRecruit-express-app/models/post.js
@@ -23,30 +23,3 @@ export const Post = sequelize.define('Post', {
     allowNull: false
   }
 });
-
-// export const Post = sequelize.define('Post', {
-//   title: {
-//     type: DataTypes.STRING,
-//     allowNull: false
-//   },
-//   content: {
-//     type: DataTypes.TEXT,
-//     allowNull: false
-//   },
-//   description: {
-//     type: DataTypes.TEXT,
-//     allowNull: false
-//   },
-//   categories: {
-//     type: DataTypes.ARRAY(DataTypes.STRING),
-//     allowNull: false
-//   },
-//   numOfApplicants: {
-//     type: DataTypes.STRING,
-//     allowNull: false
-//   },
-//   recruiters: {
-//     type: DataTypes.ARRAY(DataTypes.STRING),
-//     allowNull: false
-//   }
-// });
