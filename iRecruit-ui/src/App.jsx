6eab752d27876e284c116ef12061e33eb21588ac
Lee Fu <leefu@fb.com>
2023-07-20 15:25:25 -0700

    New comment for test

--- iRecruit-ui/src/App.jsx
+++ iRecruit-ui/src/App.jsx
@@ -45,7 +45,7 @@ function App() {
   </div>
 
   );
-
+  /* I am a new comment */
 } //bc user is a state var, once set by login, the splash page blocker line becomes true and loads Main
 
 export default App;
