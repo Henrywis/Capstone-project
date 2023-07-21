26a5fddfb04833feb591a4cbb7fd4a3e981cefce
Henrywis <alumonahenry7@gmail.com>
2023-07-21 02:00:33 -0700

    Designed ui for status,recommendation bar

---
+++ iRecruit-ui/src/components/FixedSidebar/FixedSidebar.jsx
@@ -0,0 +1,92 @@
+// import React from "react";
+// import "./FixedSidebar.css";
+
+// const FixedSidebar = () => {
+//   return (
+//     <div className="fixed-sidebar">
+//       <div className="sidebar-container">
+//         <div className="sidebar-box">
+//           <div className="box-title">
+//             <h2>Application status</h2>
+//           </div>
+//           <div className="sidebar-box1">
+//             {/* content */}
+//           </div>
+//         </div>
+//         <div className="sidebar-box">
+//           <div className="box-title">
+//             <h2>Recommendations</h2>
+//           </div>
+//           <div className="sidebar-box2">
+//             {/* content */}
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default FixedSidebar;
+
+
+import React from "react";
+import "./FixedSidebar.css";
+
+const FixedSidebar = () => {
+  // Random data for the bar chart for app status to test the UI
+  const shortlistedCount = Math.floor(Math.random() * 15);
+  const pendingCount = Math.floor(Math.random() * 20);
+  const rejectedCount = Math.floor(Math.random() * 15);
+
+  return (
+    <div className="fixed-sidebar">
+      <div className="sidebar-container">
+        <div className="sidebar-box">
+          <div className="box-title">
+            <h2>Application status</h2>
+          </div>
+          <div className="sidebar-box1">
+            {/* Part 1: Status */}
+            <div className="status-container">
+              <div className="status-item">
+                <span>Shortlisted</span>
+                <div className="status-box green"></div>
+              </div>
+              <div className="status-item">
+                <span>Pending</span>
+                <div className="status-box amber"></div>
+              </div>
+              <div className="status-item">
+                <span>Rejected</span>
+                <div className="status-box red"></div>
+              </div>
+            </div>
+
+            {/* Part 2: Bar Chart */}
+            <div className="bar-chart">
+              <div className="bar green" style={{ width: `${shortlistedCount}%` }}>
+                {shortlistedCount}
+              </div>
+              <div className="bar amber" style={{ width: `${pendingCount}%` }}>
+                {pendingCount}
+              </div>
+              <div className="bar red" style={{ width: `${rejectedCount}%` }}>
+                {rejectedCount}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="sidebar-box">
+          <div className="box-title">
+            <h2>Recommendations</h2>
+          </div>
+          <div className="sidebar-box2">
+            <p>loading recommendations here</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FixedSidebar;
