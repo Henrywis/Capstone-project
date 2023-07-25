255c7014f0aa8682e0c60f71037b1f8917bbac7a
Henrywis <alumonahenry7@gmail.com>
2023-07-24 22:01:21 -0700

    Implemented Hover for more information

--- iRecruit-ui/src/components/Home/Home.jsx
+++ iRecruit-ui/src/components/Home/Home.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./Home.css";
 import Room from "../Room/Room";
 
-export default function Home({ 
+export default function Home({
   posts,
   categoryFilter,
   selectedPostId,
@@ -11,6 +11,9 @@ export default function Home({
 }) {
   const [searchValue, setSearchValue] = useState("");
   const [filteredPosts, setFilteredPosts] = useState([]);
+
+  const [hoveredPost, setHoveredPost] = useState(null);
+  const [showSummary, setShowSummary] = useState(false);
 
   const [carouselIndex, setCarouselIndex] = useState(0);
 
@@ -21,23 +24,16 @@ export default function Home({
     "Kindly recommend this site to other potential users"
   ];
 
-
-  useEffect(() => {
-    filterPosts(posts, searchValue, categoryFilter);
-  }, [posts, searchValue, categoryFilter]);
-
+  // Function to handle the search input
   const handleSearch = (event) => {
     const searchInput = event.target.value;
     setSearchValue(searchInput);
   };
 
+  // Function to filter the posts based on search and category
   useEffect(() => {
-    const interval = setInterval(() => {
-      setCarouselIndex((prevIndex) => (prevIndex + 1) % carouselContent.length);
-    }, 7800); // Change carousel content every 5 seconds
-
-    return () => clearInterval(interval);
-  }, []); 
+    filterPosts(posts, searchValue, categoryFilter);
+  }, [posts, searchValue, categoryFilter]);
 
   const filterPosts = (posts, searchInput) => {
     const filteredPosts = posts.filter((post) => {
@@ -49,14 +45,21 @@ export default function Home({
     setFilteredPosts(filteredPosts);
   };
 
+  // Function to handle clicking on "Open Summary" button
+  const handleToggleSummary = () => {
+    setShowSummary((prevShowSummary) => !prevShowSummary);
+  };
+
+  // Function to handle clicking on "Start Application" button
   const handleStartApplication = (jobId) => {
     setSelectedPostId(jobId);
   };
 
+  // Function to handle clicking on "Close Application" button
   const handleCloseApplication = () => {
     setSelectedPostId(null);
   };
-  
+
   return (
     <div className="home">
 
@@ -79,16 +82,38 @@ export default function Home({
           placeholder="Search jobs"
         />
       </div>
+
       <div className="posts-container">
         {filteredPosts.length === 0 ? (
           <p>No jobs found matching the search criteria.</p>
         ) : (
           filteredPosts.map((job) => (
-            <div className="post" key={job.slug}>
+            <div
+              className="post"
+              key={job.slug}
+              onMouseEnter={() => setHoveredPost(job.slug)}
+              onMouseLeave={() => setHoveredPost(null)}
+            >
+             
               <h2>{job.title}</h2>
               <h3>{job.company}</h3>
               <p>Location: {job.location}</p>
               <p>Date Added: {job.dateAdded}</p>
+
+              {/* show job summary when hovered and "Open Summary" button is clicked */}
+              {hoveredPost === job.slug && (
+                <>
+                  <div className="open-summary" onClick={handleToggleSummary}>
+                    {showSummary ? "Close" : "Open Summary"}
+                  </div>
+                  {showSummary && (
+                    <div className="job-summary">
+                      <p>{job.summary}</p>
+                    </div>
+                  )}
+                </>
+              )}
+
               {selectedPostId === job.slug ? (
                 <>
                   <button onClick={handleCloseApplication}>
@@ -113,3 +138,4 @@ export default function Home({
     </div>
   );
 }
+
